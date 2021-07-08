7e52142139c9411d700cddb76f1962ea3edd90d7
Wendell Priebe <wendel_oliveira94@hotmail.com>
2021-07-08 20:10:02 -0300

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import './styles/global.scss';
 import {RepositoryList} from './components/RepositoryList.jsx'
+import {Counter} from './components/Counter.jsx';
 
 
 export function App(){
-  return <RepositoryList />;
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    
+    </>
+    )
 
 }
